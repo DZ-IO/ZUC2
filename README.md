f2865beb32e45158cfdc41871f027ecbe56aeca6
大泽 <q781000648@qq.com>
2020-01-11 11:19:38 +0800

    2.2.0
    打“包”了ZUC2

--- README.md
+++ README.md
@@ -7,7 +7,10 @@
 `# apt-get update \`  
 `&& apt-get install -y python3 git python3-pip`
 2. git clone!  
-`$ git clone https://github.com/daze456/ZdaUTAUCore2.git && cd ZdaUTAUCore2`
+`$ git clone https://github.com/daze456/ZUC2.git && cd ZUC2`
+3. 安装必要的模块
+`$ pip3 install configparser \`
+`&& git clone https://github.com/daze456/easyust.git`
 #### 0x02 准备音源：  
 1. 从[这里](https://daze456.github.io/zew/data/ZeW_Bata_0.1.0.191225.7z)下载泽小白数据  
 2. 解压缩到仓库下的voice文件夹  
@@ -35,15 +38,13 @@ ps:也可以使用命令：
 ~~`&& cd build && make \`~~  
 ~~`&& cd ../ && cd ../ && cd ../`~~  
 由于Linux版RUCE存在~~BUG~~特性，所以这里使用Windows版RUCE  
-`$ sudo apt-get install wine \`  
+`$ sudo apt-get install -y wine \`  
 `&& wget http://rocaloid.github.io/resources/binaries/RUCE-1.0.0-alpha2.zip \`  
 `&& unzip RUCE-1.0.0-alpha2.zip -d ./engine \`  
 `&& rm RUCE-1.0.0-alpha2.zip`  
 #### 0x05 合成
 `$ python3 zuc.py example.ust`  
 输出文件为`output.wav`  
-
-话说各位注意到那个`dockerfile`了吗？（可能有BUG，所以没公开）  
 ## 配置文件
 配置文件:`cfg.ini`,跟UC1差不多  
 `;泽大U核心配置文件开始`  
@@ -67,7 +68,8 @@ ps:也可以使用命令：
 `output：输出文件`
 ## 使用例：
 1. 类似口袋歌姬的在线合成服务
-2. 在游戏 OR 软件中使用UTAU的技术（类似VOCALOID SDK for Unity）
+2. 在游戏 OR 软件中使用UTAU的技术（类似VOCALOID SDK for Unity）  
+(2.2.0更新了SDK功能，[点此查看SDK](./SDK.md))  
 ## 灵感
 最初的设计灵感见[UC1.md](./UC1.md#开发灵感)  
 然后，UC1开发出来没几天，我的SSD就挂（die）了  
--- zuc.py
+++ zuc.py
@@ -1,65 +1,94 @@
 #/usr/python3
 # -*- coding:utf-8 -*-
 
-import sys,configparser,os,easyust
+import sys,configparser,os
+import easyust.easyust as easyust
+
+def wavtool(bin,output,input,stp,length,tempo,p,debug = 'no') :
+    execmd = bin + ' ' + output + ' ' + input + ' ' + stp + ' ' + length + '@' + tempo + '+.0 ' + p
+    if debug == 'yes' :
+        print('wavtool：',bin)
+        print('输出：',output)
+        print('输入：',input)
+        print('stp：',stp)
+        print('长度：',length)
+        print('节奏：',tempo)
+        print('执行：',execmd)
+    os.system(execmd)
+
+def resamp(bin,output,input,note,vel,debug = 'no') :
+    execmd = bin + ' ' + input + ' ' + output + ' ' + note + ' ' + vel
+    if debug == 'yes' :
+        print('resamper：',bin)
+        print('输出：',output)
+        print('输入：',input)
+        print('音符：',note)
+        print('速度：',vel)
+        print('执行：',execmd)
+    os.system(execmd)
+
+def synth(bt1,bt2,output,oto,tmpd,stp,length,tempo,p1,note,vel,num,lyric,debug = 'no'):
+    #重采样
+    rin = oto + '/' + lyric + '.wav'
+    rou = tmpd + '/' + num + '_' + lyric + '_' + note + '.wav'
+    resamp(bt2,rou,rin,note,vel,debug)
+    #合成
+    wavtool(bt1,output,rou,stp,length,tempo,p1,debug)
+
+def zucs(t1,t2,out,ust,oto,tmp,debug = 'no') :
+    if debug == 'yes' :
+        print('调试信息：')
+        print('wavtool：', t1)
+        print('resamper：',t2)
+        print('UST：', ust)
+        print('音源：', oto)
+        print('输出文件：',out)
+        print('临时文件：',tmp)
+    print('正在转码。。。')
+    cproject = easyust.cproject(ust,tmp + '/temp.ini')
+    if debug == 'yes': print('easyust工程：',cproject[1])
+    allnote = easyust.rallnote(cproject[1])
+    notenum = easyust.rnallnote(cproject[1])
+    for x in allnote:
+        print(str(allnote.index(x)+1),'/',notenum)
+        note = easyust.rnote(ust,x)
+        if debug == 'yes': print('音符数据：',note)
+        if note[2] == 'R': wavtool(t1,out,oto + "/R.wav",'0',note[0],note[1],'0 0',debug)
+        else : synth(t1,t2,out,oto,tmp,'0',note[0],note[1],'10 139 35 101 101 101 0 0 0',note[4],note[3],x,note[2],debug)
 
 if len(sys.argv) == 1:
-    print('[INFO] 用法：python3 zuc.py <UST> [Singer] [output]')
+    print ('用法：python3 zuc.py <ust> [singer] [output]')
 else:
+    #ust
+    ust = os.path.abspath(sys.argv[1])
+    #配置文件
+    cfg = os.path.dirname(os.path.abspath(__file__)) + '/cfg.ini'
+    #读取配置文件
     conf = configparser.ConfigParser()
-    conf.read('cfg.ini')
+    conf.read(cfg)
+    #音源
     if len(sys.argv) == 3:
-        oto = os.path.abspath(sys.argv[2])
+        singer = os.path.abspath(sys.argv[2])
     else:
-        oto = os.path.abspath(conf.get('zuc','oto'))
-    tool = os.path.abspath(conf.get('zuc','tool'))
-    resamp = os.path.abspath(conf.get('zuc','resamp'))
-    cachedir = os.path.abspath(conf.get('zuc','cachedir'))
+        singer = os.path.abspath(conf.get('zuc','oto'))
+    #工具1（wavtool）
+    tool1 = os.path.abspath(conf.get('zuc','tool'))
+    #工具2（resamper）
+    tool2 = os.path.abspath(conf.get('zuc','resamp'))
+    #缓存
+    cache = os.path.abspath(conf.get('zuc','cachedir'))
+    #输出
     if len(sys.argv) == 4:
-        output = os.path.abspath(sys.argv[3])
+        out = os.path.abspath(sys.argv[3])
     else:
-        output = os.path.abspath(conf.get('zuc','output'))
-    print('[INFO] 系统信息：')
-    print('[INFO] 音源路径：',oto)
-    print('[INFO] wavtool路径：',tool)
-    print('[INFO] 引擎路径：',resamp)
-    print('[INFO] 缓存路径：',cachedir)
-    print('[INFO] 输出路径：',output)
-    print('[INFO] 正在转码。。。')
-    cproject = easyust.cproject(os.path.abspath(sys.argv[1]),cachedir + '/temp.ini')
-    print('[INFO] UST信息：')
-    print('[INFO] 路径：',cproject[0])
-    print('[INFO] 临时文件：',cproject[1])
-    tempo = easyust.rtempo(cproject[1])
-    print('[INFO] 节奏：',tempo)
-    print('[INFO] 工程名：', easyust.rpname(cproject[1]))
-    nallnote = easyust.rnallnote(cproject[1])
-    print('[INFO] 音符个数：', nallnote)
-    allnote = easyust.rallnote(cproject[1])
-    for x in allnote:
-        nownote = str(allnote.index(x)+1)
-        print('[INFO] 当前进度：',nownote,'/',nallnote)
-        length = easyust.rlength(cproject[1],x)
-        print('[INFO] 长度：',length)
-        lyric = easyust.rlyric(cproject[1],x)
-        print('[INFO] 歌词：',lyric)
-        if lyric == 'R':
-            execcmd = tool + ' ' + output + ' ' + oto + "/R.wav 0 " + length + '@' + tempo + '+.0 0 0'
-            print('[INFO] 执行:',execcmd)
-            os.system(execcmd)
-        else:
-            note = easyust.getnote(easyust.rNoteNum(cproject[1],x))
-            print('[INFO] 音符：',note)
-            vel = easyust.rVelocity(cproject[1],x)
-            print('[INFO] 速度：',vel)
-            temp = cachedir + '/' + nownote + '_' + lyric +'_' + note + '.wav'
-            tempin = oto + '/' + lyric +'.wav'
-            execcmd = resamp + ' ' + tempin + ' ' + temp + ' ' + note + ' ' + vel
-            print('[INFO] 执行:',execcmd)
-            os.system(execcmd)
-            stp = '0'
-            note_length = length + '@' + tempo + '+.0'
-            p = '10 139 35 101 101 101 0 0 0'
-            execcmd = tool + ' ' + output + ' ' + temp + ' ' + stp + ' ' + note_length + ' ' + p
-            print('[INFO] 执行:',execcmd)
-            os.system(execcmd)
+        out = os.path.abspath(conf.get('zuc','output'))
+    #显示系统信息
+    print('系统信息：')
+    print('配置文件：',cfg)
+    print('输入UST：',ust)
+    print('音源路径：',singer)
+    print('工具1（wavtool）：',tool1)
+    print('工具2（resamper）：',tool2)
+    print('缓存：',cache)
+    print('输出：',out)
+    zucs(tool1,tool2,out,ust,singer,cache)
